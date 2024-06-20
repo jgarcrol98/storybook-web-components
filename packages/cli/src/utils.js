d2be7423e1be5d90f466a443b9e9020a3459d12f
Jose Manuel Garcia <jgarcrol@gmail.com>
2024-06-20 22:16:23 +0200

    fix tab spaces

--- packages/cli/src/utils.js
+++ packages/cli/src/utils.js
@@ -1,9 +1,9 @@
 export function changeToCamelCase(text) {
-    return text.split("-").reduce((a, b) => a.charAt(0).toUpperCase() + a.slice(1) + b.charAt(0).toUpperCase() + b.slice(1), "")
+  return text.split("-").reduce((a, b) => a.charAt(0).toUpperCase() + a.slice(1) + b.charAt(0).toUpperCase() + b.slice(1), "")
 }
 
 export function showIntroCLI() {
-    console.log(`
+  console.log(`
             ██  ██████   █████  ██████   ██████ ██████   ██████  ██           ██████ ██      ██ 
             ██ ██       ██   ██ ██   ██ ██      ██   ██ ██    ██ ██          ██      ██      ██ 
             ██ ██   ███ ███████ ██████  ██      ██████  ██    ██ ██          ██      ██      ██ 
